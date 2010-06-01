495494126ff33559558a5e85e0da045087315e95
andy <github@anarkystic.com>
2010-06-01 12:42:14 +0100

    add submodule for node-xunit project and point tests.js at it

--- tests.js
+++ tests.js
@@ -1,57 +1,10 @@
+
+require.paths.unshift('./third_party/node-unittest/lib');
+
+var unittest = require('unittest');
 var assert = require('assert');
 var sys = require('sys');
-
 var c = require('./core');
-
-
-var TestCase = function () { };
-TestCase.prototype.setUp = function () { };
-TestCase.prototype.tearDown = function () { };
-TestCase.prototype.run = function () {
-  this.passes = [];
-  this.failures = [];
-  this.errors = [];
-  for (var k in this) {
-    if (k.match(/^test/)) {
-      this.setUp();
-      try {
-        this[k]();
-        sys.print('.');
-        this.passes.push(k);
-      } catch (e) {
-        if (e instanceof assert.AssertionError) {
-          sys.print('F');
-          this.failures.push([k, e]);
-        } else {
-          sys.print('E');
-          this.errors.push([k, e]);
-        }
-      }
-      this.tearDown();
-    }
-  }
-
-  sys.puts('');
-  for (var i in this.failures) {
-    sys.puts('-----------------------------------');
-    sys.puts('FAIL: ' + this.failures[i][0]);
-    sys.puts('-----------------------------------');
-    sys.puts(this.failures[i][1]);
-  }
-
-  for (var i in this.errors) {
-    sys.puts('-----------------------------------');
-    sys.puts('ERROR: ' + this.errors[i][0]);
-    sys.puts('-----------------------------------');
-    sys.puts(this.errors[i][1]);
-  }
-
-  sys.puts('Results: Passed ' + this.passes.length +
-           ', Failed ' + this.failures.length +
-           ', Errors ' + this.errors.length);
-};
-
-
 
 
 var WarriorTestImp1 = new c.Warrior(
@@ -61,12 +14,8 @@ var WarriorTestImp2 = new c.Warrior(
     'testImp2', 0, [[c.MOV|c.I, [c.IMMEDIATE, 0], [c.IMMEDIATE, 1]]]);
 
 
-
-
-
-
 var CoreBasicTestCase = function () { };
-CoreBasicTestCase.prototype = new TestCase();
+CoreBasicTestCase.prototype = new unittest.TestCase();
 
 CoreBasicTestCase.prototype.testInitialize = function () {
   var testsize = 300;
