0ca575fb8e5ed0cdbcc8a38d6e874c181f39b95d
andy <github@anarkystic.com>
2010-11-13 22:04:02 +0900

    make tests actually run -woops-

--- tests/core_unittest.js
+++ tests/core_unittest.js
@@ -1,32 +1,83 @@
-
+var util = require('util');
 var unittest = require('unittest');
 var c = require('corewar');
 
+// a couple warriors as described at http://vyznev.net/corewar/guide.html
 
-var WarriorTestImp1 = new c.Warrior(
-    'testImp1', 0, [[c.MOV|c.I, [c.IMMEDIATE, 0], [c.IMMEDIATE, 1]]]);
+var Imp = function () {
+  return new c.Warrior(
+    'imp',
+    0,
+    [
+      [c.MOV|c.I, [c.DIRECT, 0], [c.DIRECT, 1]]
+    ]
+  );
+};
 
-var WarriorTestImp2 = new c.Warrior(
-    'testImp2', 0, [[c.MOV|c.I, [c.IMMEDIATE, 0], [c.IMMEDIATE, 1]]]);
 
+var Dwarf = function () { 
+  return new c.Warrior(
+    'dwarf',
+    0,
+    [
+      [c.ADD, [c.IMMEDIATE, 4], [c.DIRECT, 3]],
+      [c.MOV, [c.DIRECT, 2], [c.INDIRECT|c.B, 2]],
+      [c.JMP, [c.DIRECT, -2], [c.IMMEDIATE, 0]],
+      [c.DAT, [c.IMMEDIATE, 0], [c.IMMEDIATE, 0]],
+    ]
+  );
+};
 
-var CoreBasicTestCase = function () { };
+var CoreBasicTestCase = function () {
+  unittest.TestCase.apply(this, arguments);
+};
 CoreBasicTestCase.prototype = new unittest.TestCase();
+CoreBasicTestCase.prototype.setUp = function () {
+
+};
 
 CoreBasicTestCase.prototype.testInitialize = function () {
   var testsize = 300;
   var core = new c.Core({coresize: testsize});
   core.initialize();
-  assert.equal(core.core.length, testsize);
+  this.assertEqual(core.core.length, testsize);
   for (var i in core.core) {
-    assert.deepEqual(core.core[i],
+    this.assertDeepEqual(core.core[i],
                      ([c.DAT, [c.IMMEDIATE, 0], [c.IMMEDIATE, 0]]));
   }
 };
 
 CoreBasicTestCase.prototype.testLoadWarriors = function () {
-  var core = new c.Core({});
-  core.loadWarriors([WarriorTestImp1, WarriorTestImp2]);
+  var coresize = 400;
+  var mindistance = 100;
+  var core = new c.Core({coresize: coresize, mindistance: mindistance});
+  var imp = Imp();
+  var dwarf = Dwarf();
+
+  core.initialize();
+  core.loadWarriors([imp, dwarf]);
+
+  this.assertEqual(core.processes.length, 2);
+  var imp_pos = null;
+  var dwarf_pos = null;
+
+  for (var k in core.core) {
+    if (core.core[k] == imp.instructions[0]) imp_pos = k;
+    if (core.core[k] == dwarf.instructions[0]) dwarf_pos = k;
+  }
+  
+  this.assertOk((Math.abs(imp_pos - dwarf_pos) >= mindistance));
+  this.assertEqual(imp.processes[0].position, imp_pos);
+  this.assertEqual(dwarf.processes[0].position, dwarf_pos);
+
+  core.runOnce(true);
+  core.runOnce(true);
+  core.runOnce(true);
+  core.runOnce(true);
+  core.runOnce(true);
+  core.runOnce(true);
+  core.runOnce(true);
+  core.runOnce(true);
 };
 
 exports.CoreBasicTestCase = CoreBasicTestCase;
--- tests/pmars_unittest.js
+++ tests/pmars_unittest.js
@@ -1,13 +1,15 @@
 var unittest = require('unittest');
 var pmars = require('pmars');
 
-var PmarsTestCase = function () { };
+var PmarsTestCase = function () { 
+  unittest.TestCase.apply(this, arguments);
+};
 PmarsTestCase.prototype = new unittest.TestCase();
 
 PmarsTestCase.prototype.testAssemble = function () {
-  var validate = __dirname + '/third_party/pmars-0.9.2/warriors/validate.red';
-  var assembler = pmars.Assembler();
-  var parsed = assembler.Assemble(0, validate);
+  var validate = __dirname + '/../third_party/pmars-0.9.2/warriors/validate.red';
+  var assembler = new pmars.Assembler();
+  var parsed = assembler.assemble(0, validate);
 };
 
 exports.PmarsTestCase = PmarsTestCase;
